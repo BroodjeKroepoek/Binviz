3c9aa738325f023190530d177125e091da799563
Arno Werkman <awerkman1910@gmail.com>
2023-10-27 22:16:41 +0200

    Ready. Set. Go!

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+use std::{collections::BTreeMap, fmt::Debug, fs::File, io::Read, path::Path};
+
+/// An histogram of bytes.
+type Histogram = BTreeMap<u8, usize>;
+
+/// Calculate the histogram of bytes of a given file.
+pub fn calculate_histogram<P>(file: P) -> Histogram
+where
+    P: AsRef<Path> + Debug,
+{
+    let mut histogram = BTreeMap::new();
+    let mut handle = File::open(&file).expect(&format!("Couldn't open file: {:?}", file));
+    let mut buf = Vec::new();
+    handle
+        .read_to_end(&mut buf)
+        .expect(&format!("Couldn't `read_to_end` on: {:?}", handle));
+    for byte in buf {
+        histogram.entry(byte).and_modify(|x| *x += 1).or_insert(1);
+    }
+    histogram
+}
+
+/// Calculate the entropy from a given histogram.
+pub fn calculate_entropy(histogram: &Histogram) -> f64 {
+    let mut entropy = 0.0;
+    let total: usize = histogram.values().sum();
+    for (_byte, freq) in histogram {
+        let relative_freq = (*freq as f64) / (total as f64);
+        entropy += relative_freq.log2() * relative_freq;
+    }
+    -entropy
+}
+
+/// Get the top `count` most frequent byte(s) from the given histogram.
+pub fn get_most_frequent_bytes(histogram: &Histogram, count: Option<u8>) -> Vec<(&u8, &usize)> {
+    let mut vector: Vec<(&u8, &usize)> = histogram.into_iter().collect();
+    vector.sort_by(|x, y| x.1.cmp(y.1).reverse());
+    if let Some(count) = count {
+        vector.into_iter().take(count.into()).collect()
+    } else {
+        vector.into_iter().take(255).collect()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,102 @@
+use clap::Parser;
+use clap_derive::{Parser, Subcommand};
+use std::{collections::BTreeMap, fmt::Debug, fs::File, io::Read, path::PathBuf};
+
+use comfy_table::{presets::ASCII_MARKDOWN, Table};
+use image::{ImageBuffer, Luma};
+
+use binviz::{calculate_entropy, calculate_histogram, get_most_frequent_bytes};
+
+#[derive(Debug, Clone, Subcommand)]
+enum CliCommand {
+    /// Calculate the entropy of a given file in bits per byte.
+    Entropy {
+        #[arg(short, long)]
+        file: PathBuf,
+    },
+    /// Calculate the histogram of bytes of a given file.
+    Histogram {
+        #[arg(short, long)]
+        file: PathBuf,
+    },
+    /// Get the top `count` most frequent bytes or all in sorted order if `None` of a given file.
+    Frequency {
+        #[arg(short, long)]
+        file: PathBuf,
+        #[arg(short, long)]
+        count: Option<u8>,
+    },
+    /// Visualize the given file as an image (DiGraph analysis).
+    ///
+    /// We scan pair of bytes from the file and treat that as x and y coordinates into the image.
+    /// The pixels brightness will correspond with how many occurrences we have.
+    Visualize {
+        #[arg(short, long)]
+        file: PathBuf,
+    },
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: CliCommand,
+}
+
+fn main() {
+    let args = Cli::parse();
+    match args.command {
+        CliCommand::Entropy { file } => {
+            let histogram = calculate_histogram(file);
+            let entropy = calculate_entropy(&histogram);
+            println!("{} / 8.0", entropy);
+        }
+        CliCommand::Histogram { file } => {
+            let histogram = calculate_histogram(file);
+            println!("{:?}", histogram)
+        }
+        CliCommand::Frequency { file, count } => {
+            let histogram = calculate_histogram(file);
+            let most_freq = get_most_frequent_bytes(&histogram, count);
+            let total: usize = histogram.values().sum();
+            let mut table = Table::new();
+            table.load_preset(ASCII_MARKDOWN);
+            table.set_header(["Rank", "Byte", "Hex", "Text", "Frequency"]);
+            for (i, (byte, freq)) in most_freq.into_iter().enumerate() {
+                let relative_freq = (*freq as f64) / (total as f64);
+                table.add_row([
+                    format!("{}", i),
+                    format!("{}", byte),
+                    format!("{:#x}", byte),
+                    format!("{:?}", *byte as char),
+                    format!("{}", relative_freq),
+                ]);
+            }
+            println!("{}", table);
+        }
+        CliCommand::Visualize { file } => {
+            let mut two_pairs: BTreeMap<(u8, u8), usize> = BTreeMap::new();
+            let mut handle = File::open(&file).expect(&format!("Couldn't open file: {:?}", file));
+            let mut bytes = Vec::new();
+            handle
+                .read_to_end(&mut bytes)
+                .expect(&format!("Couldn't `read_to_end` on: {:?}", handle));
+            for slice in bytes.windows(2) {
+                two_pairs
+                    .entry((slice[0], slice[1]))
+                    .and_modify(|x| *x += 1)
+                    .or_insert(1);
+            }
+            let mut image = ImageBuffer::new(256, 256);
+            let len = two_pairs.values().len();
+            let total = (two_pairs.values().sum::<usize>() as f64) / (len as f64);
+            for (x, y) in two_pairs.keys() {
+                if let Some(freq) = two_pairs.get(&(*x, *y)) {
+                    let brightness = (*freq as f64) / total * (u16::MAX as f64);
+                    let pixel = Luma([brightness as u16]);
+                    image.put_pixel(*x as u32, *y as u32, pixel);
+                }
+            }
+            image.save("output.png").expect("Couldn't save image");
+        }
+    }
+}
