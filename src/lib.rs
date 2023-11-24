c2590151225b39001892c7ca0158c0fd56fbd005
Arno Werkman <awerkman1910@gmail.com>
2023-11-24 13:59:50 +0100

    Add logging for info and performance tracking

--- src/lib.rs
+++ src/lib.rs
@@ -31,43 +31,47 @@ where
     histogram
 }
 
+#[inline(always)]
+pub fn calculate_entropy(probability: f64) -> f64 {
+    probability.log2() * probability
+}
+
 /// Calculate the entropy from a given n-dimensional histogram.
-pub fn calculate_entropy(histogram: &Histogram<u8>) -> f64 {
+pub fn calculate_entropy_histogram(histogram: &Histogram<u8>) -> f64 {
     let total: usize = histogram.values().sum();
     let entropy = histogram
         .iter()
         .map(|(_, freq)| {
-            let relative_freq = (*freq as f64) / (total as f64);
-            relative_freq.log2() * relative_freq
+            let probability = (*freq as f64) / (total as f64);
+            calculate_entropy(probability)
         })
         .sum::<f64>();
     -entropy
 }
 
-// TODO: Remove this?
-/// Get the top `count` most frequent byte(s) from the given n-dimensional histogram.
 pub fn get_most_frequent_bytes(histogram: &Histogram<u8>) -> Vec<(&Vec<u8>, &usize)> {
     let mut vector: Vec<(&Vec<u8>, &usize)> = histogram.into_iter().collect();
     vector.sort_by(|x, y| y.1.cmp(x.1));
     vector
 }
 
-pub fn display_entropies(entropy: f64, causal_entropy: f64) -> String {
-    let rel_entropy = entropy / 8.0;
-    let rel_causal_entropy = causal_entropy / 16.0;
+pub fn display_entropies<P>(file: P, count: usize) -> String
+where
+    P: AsRef<Path> + Debug,
+{
     let mut table = Table::new();
     table.load_preset(ASCII_MARKDOWN);
-    table.set_header(["Type", "Value", "Relative"]);
-    table.add_row([
-        "entropy",
-        &format!("{:.5} (bits/byte)", entropy),
-        &format!("{:.5}", rel_entropy),
-    ]);
-    table.add_row([
-        "causal entropy",
-        &format!("{:.5} (bits/dword)", causal_entropy),
-        &format!("{:.5}", rel_causal_entropy),
-    ]);
+    table.set_header(["Dimension", "Entropy", "Relative Entropy"]);
+    for i in 1..=count {
+        let histogram = calculate_histogram(&file, i);
+        let entropy = calculate_entropy_histogram(&histogram);
+        let rel_entropy = entropy / (8.0f64 * (i as f64));
+        table.add_row([
+            format!("{}", i),
+            format!("{:.5} (bits per {} byte(s))", entropy, i),
+            format!("{:.5}", rel_entropy),
+        ]);
+    }
     table.to_string()
 }
 
@@ -79,33 +83,37 @@ pub fn display_most_frequent(histogram: &Histogram<u8>) -> String {
     table.load_preset(ASCII_MARKDOWN);
     table.set_header(["Rank", "Byte", "Hex", "Text", "Relative Frequency"]);
     for (i, (byte, freq)) in most_freq.into_iter().enumerate() {
-        let relative_freq = (*freq as f64) / (total as f64);
+        let probability = (*freq as f64) / (total as f64);
         table.add_row([
             format!("{}", i),
             format!("{}", byte[0]),
             format!("{:#x}", byte[0]),
             format!("{:?}", byte[0] as char),
-            format!("{:.5}", relative_freq),
+            format!("{:.5}", probability),
         ]);
     }
     table.to_string()
 }
 
-pub fn generate_image(dihistogram: &Histogram<u8>) -> ImageBuffer<Luma<u16>, Vec<u16>> {
+pub fn generate_image(
+    dihistogram: &Histogram<u8>,
+) -> (ImageBuffer<Luma<u16>, Vec<u16>>, usize, f64) {
     debug_assert!(dihistogram.into_iter().all(|x| x.0.len() == 2));
     let mut image = ImageBuffer::new(256, 256);
     let len = dihistogram.values().len();
-    let total = (dihistogram.values().sum::<usize>() as f64) / (len as f64);
+    let total: usize = dihistogram.values().sum();
+    let avg_total = (total as f64) / (len as f64);
     for slice in dihistogram.keys() {
         if let Some(freq) = dihistogram.get(slice) {
-            let brightness = (*freq as f64) / total * (u16::MAX as f64);
+            let brightness = (*freq as f64) / avg_total * (u16::MAX as f64);
             let pixel = Luma([brightness as u16]);
             image.put_pixel(slice[0] as u32, slice[1] as u32, pixel);
         }
     }
-    image
+    (image, total, avg_total)
 }
 
+/// Perform a full analysis on all the files provided.
 pub fn full_analysis(files: Vec<PathBuf>) {
     for file in &files {
         // Create a folder for each file to store the analysis results.
@@ -122,16 +130,12 @@ pub fn full_analysis(files: Vec<PathBuf>) {
         }
 
         // Perform the Ent subcommand.
-        let histogram = calculate_histogram(&file, 1);
-        let dihistogram = calculate_histogram(&file, 2);
-
-        let entropy = calculate_entropy(&histogram);
-        let causal_entropy = calculate_entropy(&dihistogram);
-        let entropy_output = display_entropies(entropy, causal_entropy);
+        let entropy_output = display_entropies(&file, 3);
         fs::write(output_folder.join("entropy.txt"), entropy_output)
             .expect("Couldn't write into 'entropy.txt'");
 
         // Perform the Fre subcommand.
+        let histogram = calculate_histogram(&file, 1);
         let most_frequent_output = display_most_frequent(&histogram);
         fs::write(
             output_folder.join("most_frequent.txt"),
@@ -140,11 +144,16 @@ pub fn full_analysis(files: Vec<PathBuf>) {
         .expect("Couldn't write into `most_frequent.txt`");
 
         // Perform the Vis subcommand.
-        let image = generate_image(&dihistogram);
+        let dihistogram = calculate_histogram(&file, 2);
+        let (image, total, avg_total) = generate_image(&dihistogram);
         image
             .save(output_folder.join("image.png"))
             .expect("Couldn't save image into `image.png`");
-
+        println!("[INFO] `{}` byte pairs in the visualization.", total);
+        println!(
+            "[INFO] full brightness means `{}` byte pairs at that location.",
+            avg_total
+        );
         println!("[INFO] Analysis for '{}' is complete.", file.display());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
 use clap::Parser;
 use clap_derive::{Parser, Subcommand};
-use std::{fmt::Debug, path::PathBuf};
+use comfy_table::{presets::ASCII_MARKDOWN, Table};
+use env_logger::Env;
+use log::info;
+use std::{fmt::Debug, path::PathBuf, time::Instant};
 
 use binviz::{
-    calculate_entropy, calculate_histogram, display_entropies, display_most_frequent,
-    full_analysis, generate_image,
+    calculate_entropy_histogram, calculate_histogram, display_most_frequent, full_analysis,
+    generate_image,
 };
 
 #[derive(Debug, Clone, Subcommand)]
 enum CliCommand {
-    /// Calculate the entropy and causal entropy of a given file in bits per byte and bits per dword respectively.
-    Ent {
+    /// Calculate the n-dimensional entropy of a given file, for n in 1..=count, in bits per `n` bytes.
+    Entropy {
         #[arg(short, long)]
         file: PathBuf,
+        #[arg(short, long)]
+        count: usize,
     },
-    // TODO: Add the most frequent conditional byte using a di-histogram.
     /// Get the bytes in sorted order according to their frequency of a given file.
-    Fre {
+    Frequency {
         #[arg(short, long)]
         file: PathBuf,
     },
-    // TODO: Train a NN on this, to classify file formats?
     /// Visualize the given file as an image (digraph analysis).
     ///
     /// We scan pair of bytes from the file and treat that as x and y coordinates into the image.
@@ -28,7 +31,7 @@ enum CliCommand {
     ///
     /// This can show conditional relationships within a binary file.
     /// Distinct file formats will produce distinct recognizable patterns in the image.
-    Vis {
+    Visualize {
         #[arg(short, long)]
         file: PathBuf,
     },
@@ -47,26 +50,107 @@ struct Cli {
 }
 
 fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let args = Cli::parse();
     match args.command {
-        CliCommand::Ent { file } => {
-            let histogram = calculate_histogram(&file, 1);
-            let dihistogram = calculate_histogram(&file, 2);
-            let entropy = calculate_entropy(&histogram);
-            let causal_entropy = calculate_entropy(&dihistogram);
-            println!("{}", display_entropies(entropy, causal_entropy));
+        CliCommand::Entropy { file, count } => {
+            info!("start: executing entropy subcommand...");
+            let start_entropy_command = Instant::now();
+            info!("start: initializing empty table with headers...");
+            let start_table = Instant::now();
+            let mut table = Table::new();
+            table.load_preset(ASCII_MARKDOWN);
+            table.set_header(["Dimension", "Entropy", "Relative Entropy"]);
+            let elapsed_table = start_table.elapsed();
+            info!(
+                "end: finished initializing empty table with headers, with elapsed time: {:?}",
+                elapsed_table
+            );
+            info!("start: calculating the actual entries of the table...");
+            let start_collecting = Instant::now();
+            for i in 1..=count {
+                info!("start: calculating histogram of dimension `{}`...", i);
+                let start_histogram = Instant::now();
+                let histogram = calculate_histogram(&file, i);
+                let elapsed_histogram = start_histogram.elapsed();
+                info!(
+                    "end: finished calculating histogram of dimension `{}`, with elapsed time: {:?}",
+                    i, elapsed_histogram
+                );
+                info!("start: calculating entropy of histogram...");
+                let start_calc_entropy = Instant::now();
+                let entropy = calculate_entropy_histogram(&histogram);
+                let elapsed_calc_entropy = start_calc_entropy.elapsed();
+                info!(
+                    "end: finished calculating entropy of histogram, with elapsed time: {:?}",
+                    elapsed_calc_entropy
+                );
+                info!(
+                    "start: additionally calculating relative entropy and adding entry to table..."
+                );
+                let start_entry_add = Instant::now();
+                let rel_entropy = entropy / (8.0f64 * (i as f64));
+                table.add_row([
+                    format!("{}", i),
+                    format!("{:.5} (bits per {} byte(s))", entropy, i),
+                    format!("{:.5}", rel_entropy),
+                ]);
+                let elapsed_entry_add = start_entry_add.elapsed();
+                info!("end: finished calculating relative entropy and adding entry to table, with elapsed time: {:?}", elapsed_entry_add);
+            }
+            let elapsed_collecting = start_collecting.elapsed();
+            info!(
+                "end: finished collecting the actual entries of the table, with elapsed time: {:?}",
+                elapsed_collecting
+            );
+            let elapsed_entropy_command = start_entropy_command.elapsed();
+            info!(
+                "end: finished executing entropy subcommand, with elapsed time: {:?}",
+                elapsed_entropy_command
+            );
+            println!("{}", table);
         }
-        CliCommand::Fre { file } => {
+        CliCommand::Frequency { file } => {
+            info!("start: executing frequency subcommand...");
+            let start_freq_command = Instant::now();
+
+            info!("start: calculating histogram...");
+            let start_histogram = Instant::now();
             let histogram = calculate_histogram(&file, 1);
+            let elapsed_histogram = start_histogram.elapsed();
+            info!(
+                "end: finished calculating histogram, with elapsed time: {:?}",
+                elapsed_histogram
+            );
+            let elapsed_freq_command = start_freq_command.elapsed();
+            info!(
+                "end: finished executing frequency subcommand, with elapsed time: {:?}",
+                elapsed_freq_command
+            );
             println!("{}", display_most_frequent(&histogram));
         }
-        CliCommand::Vis { file } => {
+        CliCommand::Visualize { file } => {
+            info!("start: executing visualize subcommand...");
+            let start_vis_command = Instant::now();
+            info!("calculating histogram...");
             let dihistogram = calculate_histogram(&file, 2);
-            println!("[INFO] generating image...");
-            let image = generate_image(&dihistogram);
-            println!("[INFO] finished");
+            info!("finished calculating histogram.");
+            info!("generating image...");
+            let (image, total, avg_total) = generate_image(&dihistogram);
+            info!("finished generating image.");
+            info!("saving image to `.\\output.png`...");
             image.save("output.png").expect("Couldn't save image");
-            println!("[INFO] image saved to '.\\output.png'.");
+            info!("image saved to '.\\output.png'.");
+            info!("`{}` byte pairs visualized.", total);
+            info!(
+                "full brightness means `{:.4}` byte pairs at that location.",
+                avg_total
+            );
+            let elapsed_vis_command = start_vis_command.elapsed();
+            info!(
+                "end: finished executing visualize subcommand, with elapsed time: {:?}",
+                elapsed_vis_command
+            );
         }
         CliCommand::Full { files } => full_analysis(files),
     }
